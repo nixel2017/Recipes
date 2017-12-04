882feb73aeae7880f6883b95a04f8695a7786441
Nicholas Loya <elesen@carolina.rr.com>
2017-12-04 21:15:23 +0000

    Incomplete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,17 @@ class TasksController < ApplicationController
     def create
         @guide = Guide.find(params[:guide_id])
         @task = @guide.tasks.create(task_params)
-        redirect_to edit_guide_path(@guide)
+        redirect_to guide_path(@guide)
     end
-    
+    def destroy
+        @task = Guide.find(params[:id])
+        @task.destroy
+        
+        redirect_to guides_path
+    end
     private
         def task_params
             params.require(:task).permit(:body)
         end
+        
 end
