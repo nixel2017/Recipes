781f7f72d0024beb0ef89a27799b5543b5debe52
Nicholas Loya <elesen@carolina.rr.com>
2017-12-06 20:56:13 +0000

    Fixed problems and added website name

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,15 +4,22 @@ class TasksController < ApplicationController
         @task = @guide.tasks.create(task_params)
         redirect_to guide_path(@guide)
     end
+    
+
     def edit
         @guide = Guide.find(params[:id])
     end
+    
+   
+
     def destroy
         @guide = Guide.find(params[:guide_id])
         @task = @guide.tasks.find(params[:id])
         @task.destroy
         redirect_to edit_guide_path(@guide)
     end
+    
+
     private
         def task_params
             params.require(:task).permit(:body)
